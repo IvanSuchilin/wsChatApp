726b0db905203128d0a8801466fce90d4c73ec0e
Ivan Suchilin <104725388+IvanSuchilin@users.noreply.github.com>
2024-01-13 00:15:23 +0300

    initial

---
+++ src/main/java/com/sin/wschatapp/WschatappApplication.java
@@ -0,0 +1,13 @@
+package com.sin.wschatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WschatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WschatappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sin/wschatapp/WschatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sin.wschatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WschatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
